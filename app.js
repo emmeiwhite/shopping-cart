203464e1f4493c420527fabc0351f2980f1c660a
Imran Rather <emmeiwhite@gmail.com>
2022-01-02 23:57:13 +0530

    Setting cart fxnality WIP

--- app.js
+++ app.js
@@ -10,8 +10,10 @@ const cartContent = document.querySelector(".cart-content");
 const productsDOM = document.querySelector(".products-center");
 
 // cart
-
 let cart = []; // Manipuate data from LocalStorage and back
+
+// buttons
+let buttonsDOM = [];
 
 // getting the products
 class Products {
@@ -43,7 +45,6 @@ class Products {
 class UI {
   displayProducts(products) {
     let result = "";
-    console.log(products);
     products.forEach(({ image, id, price, title }) => {
       result += `
         <!-- Single Product -->
@@ -66,15 +67,69 @@ class UI {
 
     productsDOM.innerHTML = result;
   }
+
+  getBagButtons() {
+    const buttons = [...document.querySelectorAll(".bag-btn")]; // NodeList to Array
+    buttonsDOM = buttons; // I didn't get this yet
+    buttons.forEach((btn) => {
+      let id = btn.dataset.id;
+      // localStorage check on cart array, since cart will be stored in localStorage
+      let inCart = cart.find((item) => item.id === id);
+
+      if (inCart) {
+        btn.innerText = "In Cart";
+        btn.disabled = true;
+      }
+
+      btn.addEventListener("click", (event) => {
+        event.target.innerText = "In Cart";
+        event.target.disabled = true;
+
+        // get product from localStorage products
+        const currentProduct = Storage.getProduct(id);
+        let cartItem = { ...currentProduct, amount: 1 };
+
+        // add product to the cart
+        cart = [...cart, cartItem];
+        // save cart in local Storage
+        Storage.saveCart(cart);
+        // set cart Values
+        // display cart item
+        // show the cart
+      });
+    });
+  }
 }
 
 // local storage
-class Storage {}
+class Storage {
+  static saveProducts(products) {
+    localStorage.setItem("products", JSON.stringify(products));
+  }
+
+  static getProduct(id) {
+    let products = JSON.parse(localStorage.getItem("products"));
+    const currentProduct = products.find((product) => product.id === id);
+    return currentProduct;
+  }
+
+  static saveCart(cart) {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }
+}
 
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("DOMContent has Loaded");
   const ui = new UI();
   const products = new Products();
 
-  products.getProducts().then((products) => ui.displayProducts(products));
+  products
+    .getProducts()
+    .then((products) => {
+      ui.displayProducts(products);
+      Storage.saveProducts(products);
+    })
+    .then(() => {
+      // to make sure first all the products are available
+      ui.getBagButtons();
+    });
 });
